1b69b316dd85fb437f881e54fafba1f1df8cce38
Kiichi Takeuchi <kiichi.takeuchi@iprocedures.com>
2015-10-14 16:52:03 -0400

    tile server changes

--- tile_server.py
+++ tile_server.py
@@ -2,13 +2,16 @@ from distutils.dir_util import mkpath
 from globalmaptiles import *
 import os
 import matplotlib
-matplotlib.rcParams['figure.figsize'] = [3.57,3.57]
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pyart
 import urllib
 from flask import Flask, request, send_file
 from flask import jsonify
+
+
+matplotlib.rcParams['figure.figsize'] = [3.57,3.57]
 
 app = Flask(__name__,static_url_path='')
 
@@ -52,21 +55,22 @@ def send_tile(z,x,y):
     tile_info = "%s: %s"%(tilefilename,bounds)
     print tile_info
     #print tilefilename, bounds[0],bounds[1],bounds[2],bounds[3]
-
+    plt.clf()
     display = pyart.graph.RadarMapDisplay(radar)
-    display.plot_ppi_map( 'reflectivity', vmin=-32, vmax=80, cmap='pyart_NWSRef',
+    display.plot_ppi_map( 'reflectivity',0, vmin=-32, vmax=80, cmap='pyart_NWSRef',
         min_lat=bounds[0],min_lon=bounds[1],  max_lat=bounds[2], max_lon=bounds[3],
         #lat_0=(bounds[0]+bounds[2])/2.0,lon_0=(bounds[1]+bounds[3])/2.0,
-        title_flag=True,title = tilefilename,
-        #title_flag=False,
+        #title_flag=True,title = tilefilename,
+        title_flag=False,
         mask_outside=True,
-        projection="lcc",
+        #projection="lcc",
         resolution='c',colorbar_flag=False, embelish=False,
         )
-    fig=plt.figure(1)
-    fig.canvas.draw()
+    #fig=plt.figure(1)
+    #fig.canvas.draw()
+    #fig.savefig(tilefilename, dpi=72, transparent=True)
 
-    fig.savefig(tilefilename, dpi=72, transparent=True)
+    plt.savefig(tilefilename, dpi=72, transparent=True,pad_inches=0)
     return send_file(tilefilename, mimetype='image/png')
 
 # DEUBUG
@@ -121,19 +125,32 @@ def get_tile_info(zoom,lat,lon):
 #http://localhost:5000/sample
 @app.route('/sample')
 def sample_tile():
+    #cache/8/75/159.png 39.9097362345 -74.53125 40.9798980696 -73.125
+    #bounds = mercator.TileLatLonBounds(75, 158, 8)
+    #print bounds[0],bounds[1],  bounds[2], bounds[3]
+    #print "min_lat=39.9097362345,min_lon=-74.53125,  max_lat=40.9798980696, max_lon=-73.125"
+    plt.clf()
     matplotlib.rcParams['figure.figsize'] = [12,12]
     tilefilename = "cache/sample.png"
     display = pyart.graph.RadarMapDisplay(radar)
     display.plot_ppi_map( 'reflectivity', vmin=-32, vmax=80, cmap='pyart_NWSRef',
-        title_flag=True,#title = tilefilename,
+        title_flag=False,#title = tilefilename,
         mask_outside=True,
-        projection="lcc",
+        min_lat=39.9097362345,min_lon=-74.53125,  max_lat=40.9798980696, max_lon=-73.125,
+        #min_lat=bounds[0],min_lon=bounds[1],  max_lat=bounds[2], max_lon=bounds[3],
+        #projection="merc",
         resolution='c',colorbar_flag=False, embelish=False,
         )
-    display.basemap.drawcounties()
-    fig=plt.figure(1)
-    fig.canvas.draw()
-    fig.savefig(tilefilename, dpi=72, transparent=True)
+
+    #display.basemap.drawcounties()
+
+    # fig=plt.figure(1)
+    # fig.canvas.draw()
+    # fig.savefig(tilefilename, dpi=72, transparent=True)
+    #fig = plt.figure(figsize=[3.57,3.57])
+    #fig.canvas.draw()
+
+    plt.savefig(tilefilename, dpi=72, transparent=True,pad_inches=0,bbox_inches=0)
     return send_file(tilefilename, mimetype='image/png')
 
 if __name__ == '__main__':
